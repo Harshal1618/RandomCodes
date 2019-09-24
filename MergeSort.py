b681b5c62b8052b3bff503d5b994795c3c608a5d
Harshal1618 <harshal1618@gmail.com>
2019-09-23 22:47:19 -0400

    Modular flow of merge sort

--- MergeSort.py
+++ MergeSort.py
@@ -1,38 +1,47 @@
-def mergeSort(alist):
-    print("Splitting ",alist)
-    if len(alist)>1:
-        mid = len(alist)//2
-        lefthalf = alist[:mid]
-        print("lefthalf ",lefthalf)
-        righthalf = alist[mid:]
-        print("righthalf ",righthalf)
 
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
+def mergeLogic(l1, l2):
+    i1 =0
+    i2 =0
+    l = []
+    n = len(l1) + len(l2)
 
-        i=0
-        j=0
-        k=0
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] <= righthalf[j]:
-                alist[k]=lefthalf[i]
-                i=i+1
-            else:
-                alist[k]=righthalf[j]
-                j=j+1
-            k=k+1
+    for ctr in range(n):
 
-        while i < len(lefthalf):
-            alist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
+        # added as a afterthought after seeing error
+        if i1 == len(l1):
+            l = l + l2[i2:]
+            break
+        if i2 == len(l2):
+            l = l + l1[i1:]
+            break
 
-        while j < len(righthalf):
-            alist[k]=righthalf[j]
-            j=j+1
-            k=k+1
-    print("Merging ",alist)
+        # writen in frst iteration
+        if l1[i1] < l2[i2]:
+            l.append(l1[i1])
+            i1 += 1
+        else :
+            l.append(l2[i2])
+            i2 += 1
 
-alist = [54,26,93,17,77,31,44,55,20]
-mergeSort(alist)
-print(alist)
+    return l
+
+def mergeSort(l):
+    mid =  int(len(l) / 2)
+    l_left = l[:mid]
+    l_right = l[mid:]
+
+    if len(l_left) > 1:
+        x_l = mergeSort(l_left)
+    else:
+        x_l = l_left
+
+    if len(l_right) > 1:
+        x_r = mergeSort(l_right)
+    else:
+        x_r = l_right
+
+    return mergeLogic(x_l, x_r)
+
+if __name__ == '__main__':
+    x = mergeSort([5,2,1,3,7,4])
+    print(x)
